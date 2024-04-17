38d8f2ab0d752a4009280fe823f5e8a0eb19f80d
sdwan <nihao.liulei@163.com>
2024-04-17 22:33:58 +0800

    Update __init__.py
    原github代码有bug无法运行

--- stun/__init__.py
+++ stun/__init__.py
@@ -90,10 +90,10 @@ ChangedAddressError = "Meet an error, when do Test1 on Changed IP and Port"
 def _initialize():
     items = dictAttrToVal.items()
     for i in range(len(items)):
-        dictValToAttr.update({items[i][1]: items[i][0]})
+        dictValToAttr.update({list(items)[i][1]: list(items)[i][0]})
     items = dictMsgTypeToVal.items()
     for i in range(len(items)):
-        dictValToMsgType.update({items[i][1]: items[i][0]})
+        dictValToMsgType.update({list(items)[i][1]: list(items)[i][0]})
 
 
 def gen_tran_id():
@@ -132,9 +132,9 @@ def stun_test(sock, host, port, source_ip, source_port, send_data=""):
                 else:
                     retVal['Resp'] = False
                     return retVal
-        msgtype = binascii.b2a_hex(buf[0:2])
+        msgtype = binascii.b2a_hex(buf[0:2]).decode("utf-8")
         bind_resp_msg = dictValToMsgType[msgtype] == "BindResponseMsg"
-        tranid_match = tranid.upper() == binascii.b2a_hex(buf[4:20]).upper()
+        tranid_match = tranid.upper() == binascii.b2a_hex(buf[4:20]).upper().decode("utf-8")
         if bind_resp_msg and tranid_match:
             recvCorr = True
             retVal['Resp'] = True
@@ -142,7 +142,7 @@ def stun_test(sock, host, port, source_ip, source_port, send_data=""):
             len_remain = len_message
             base = 20
             while len_remain:
-                attr_type = binascii.b2a_hex(buf[base:(base + 2)])
+                attr_type = binascii.b2a_hex(buf[base:(base + 2)]).decode("utf-8")
                 attr_len = int(binascii.b2a_hex(buf[(base + 2):(base + 4)]), 16)
                 if attr_type == MappedAddress:
                     port = int(binascii.b2a_hex(buf[base + 6:base + 8]), 16)
